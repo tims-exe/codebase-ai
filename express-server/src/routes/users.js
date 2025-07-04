2cc096dee4ba171c33e139e95fc9e6f0bfbd50ce
tims-exe <timonkjohn@gmail.com>
2025-07-04 23:37:09 +0530

    basic express server

--- express-server/src/routes/users.js
+++ express-server/src/routes/users.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
 
 module.exports = router;
